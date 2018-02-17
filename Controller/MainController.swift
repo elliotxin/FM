f7f3946fa9694b07e6ab008a7330b243cb2cb33e
elliotxin <elliotxin@gmail.com>
2018-02-18 00:15:38 +0800

    Initial commit

---
+++ Controller/MainController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FM
+//
+//  Created by elliot xin on 2/17/18.
+//  Copyright © 2018 elliot xin. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
